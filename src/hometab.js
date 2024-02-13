5cba2e8f83fbe9e10f0da380c6dfa0d07aa2ed40
leadgfl13 <leadgfl13@gmail.com>
2024-02-12 22:11:12 -0500

    Add functions to display html elements on page

--- src/hometab.js
+++ src/hometab.js
@@ -1,19 +1,43 @@
+
 const hello = ()=>{
     let hometab = document.getElementById('hometab')
 
     alert("This is a test, it worked!")
 }
 
+function makeHeader(){
+    let head = document.getElementById('head')
+    let header = document.createElement('div')
+    header.setAttribute('id','header')
+    head.prepend(header)
+    header.innerHTML = 'Leaders'
+}
+function addDiv(){
+    let contentarea = document.getElementById('content')
+    let thing = document.createElement('div')
+    thing.setAttribute('id', 'thing')
+    contentarea.append(thing)
+    thing.innerHTML = 'test worked'
+}
+
+
 
 const rendermain =()=>{
     let contentarea = document.getElementById('content')
     let testarea = document.createElement('div')
     testarea.setAttribute('id','mainarea')
-    testarea.innerHTML= 'hello'
-    
     contentarea.append(testarea)
+    let blank = document.createElement('div')
+    blank.setAttribute('id','blankarea')
+    testarea.append(blank)
+    makeHeader()
+    addDiv()
+    let square1 = document.createElement('div')
+    square1.setAttribute('id','greeting')
+    square1.innerHTML = 'Your taste buds will thank you'
+    blankarea.append(square1)
     
 }
 
 
-export {hello, rendermain};
+export {hello, rendermain,};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import './style.css';
 import { rendermain } from './hometab.js';
 import {hello} from './hometab.js';
+import { addDiv } from './hometab.js';
+import { addID } from './hometab.js';
+
+
 
 
  let poop = document.getElementById('hometab')
